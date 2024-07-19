88228c98ebc1f13d3102cbde5bc94592d37607cc
Dan Lepe <lepedaniel8822@gmail.com>
2024-07-19 17:12:07 -0500

    added app file

--- src/app/api/[...nextauth].ts
+++ src/app/api/[...nextauth].ts
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth";
 import { AuthOptions } from "next-auth";
 import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { Adapter } from "next-auth/adapters";
 
 const nextAuthOptions: AuthOptions = {
   providers: [],
   adapter: SupabaseAdapter({
     url: process.env.SUPABASE_URL!,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  }),
+  }) as Adapter,
 };
 export default NextAuth({
   ...nextAuthOptions
